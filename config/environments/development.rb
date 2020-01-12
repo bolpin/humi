c28ad13cefd757b1340e05960305546d484b2264
Brian Olpin <bolpin@gmail.com>
2020-01-12 12:15:44 -0800

    disable yarn integrity warning in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
+
+  config.webpacker.check_yarn_integrity = false # https://github.com/rails/webpacker/issues/1568 
 end
