bb84651ba2da062f8ad437311c8377959c641963
Brian Olpin <bolpin@gmail.com>
2020-01-12 11:06:10 -0800

    installing activeadmin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :validatable
   # devise modules available include:
   # :confirmable, :lockable, :timeoutable, :trackable, :omniauthable
-  # :registerable
+  # 
   devise :database_authenticatable,
          :recoverable,
          :rememberable,
