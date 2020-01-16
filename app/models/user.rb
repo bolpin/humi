72a984a7c21a37449963943f501b64d870b2f8e7
Brian Olpin <bolpin@gmail.com>
2020-01-16 13:37:07 -0800

    removing duplicate devise modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
-  # devise modules available include:
-  # :confirmable, :lockable, :timeoutable, :trackable, :omniauthable
-  #
-  devise :database_authenticatable,
-         :recoverable,
-         :rememberable,
-         :validatable
 end
