d37f96a80295994370179f07afdfa9c57ce0b48d
Matthew Bell <matthew.bell@gettyimages.com>
2020-01-16 12:26:56 -0800

    adding more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+#
+# Partners
+p1 = Partner.create!(name: 'Matt & Co.', address: '1234 Main St')
+p2 = Partner.create!(name: 'Matts pizza', address: '12345 Main St')
+p3 = Partner.create!(name: 'Humi', address: '123456 Main St')
+
+# Users
+User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', partner: p3) if Rails.env.development?
+User.create!(email: 'partner@example.com', password: 'password', password_confirmation: 'password', partner: p1) if Rails.env.development?
+User.create!(email: 'partner@demo.com', password: 'password', password_confirmation: 'password', partner: p2) if Rails.env.development?
+
+
+# Grants
+g1 = Grant.create!(partner: p1, amount_cents: 1000000, date: DateTime.now, case_management_percentage: 10)
+g2 = Grant.create!(partner: p2, amount_cents: 1000000, date: DateTime.now, case_management_percentage: 10)
+
+# Donations
+Donation.create!(grant: g1, donor: 'Andrew S.', amount_cents: 10000, date: DateTime.now)
+Donation.create!(grant: g1, donor: 'Brian O.', amount_cents: 20000, date: DateTime.now)
+Donation.create!(grant: g2, donor: 'Travis S.', amount_cents: 20000, date: DateTime.now)
