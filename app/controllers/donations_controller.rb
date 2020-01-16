dee9b6206414ba97ad872fafea1897f72e3d5c91
Matthew Bell <matthew.bell@gettyimages.com>
2020-01-16 10:32:19 -0800

    rails generate controller donations index show create edit destory new

---
+++ app/controllers/donations_controller.rb
@@ -0,0 +1,19 @@
+class DonationsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destory
+  end
+
+  def new
+  end
+end
