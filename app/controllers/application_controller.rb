88535d84863cc28a075fe24c8265870c02cf03bc
Travis Spangle <Travis.Spangle@GettyImages.com>
2020-01-16 14:07:05 -0800

    integrate cancan into admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  protect_from_forgery with: :null_session
+
+  def access_denied(exception)
+    redirect_to root_path, alert: exception.message
+  end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,10 +5,13 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
+
     if user.admin?
       can :manage, :all
     else
-      can :read, :all
+      can :read, User, id: user.id
+      can :manage, User, id: user.id
+      can :read, ActiveAdmin::Page, name: "Dashboard", namespace_name: "admin"
     end
   end
 end
