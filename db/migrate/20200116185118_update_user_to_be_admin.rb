260390f0ed0ae312a8908ee7649cf9b36be5d85a
Travis Spangle <Travis.Spangle@GettyImages.com>
2020-01-16 12:29:58 -0800

    Prevent nil error
    This migration is only needed for people who have already set up the db.

--- db/migrate/20200116185118_update_user_to_be_admin.rb
+++ db/migrate/20200116185118_update_user_to_be_admin.rb
@@ -1,5 +1,5 @@
 class UpdateUserToBeAdmin < ActiveRecord::Migration[6.0]
   def up
-    User.find_by_email("admin@example.com").update(admin: true)
+    User.find_by_email("admin@example.com")&.update(admin: true)
   end
 end
