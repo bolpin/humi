18d387dc1a123225682002c72999d3eacaf88b4d
Brian Olpin <bolpin@gmail.com>
2020-01-16 20:57:27 -0800

    re-adding root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # root to: "home#index"
+  root to: "home#index"
 
   resources :disbursements
   resources :donations
