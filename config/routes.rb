c10a5c201bf6b40644b96239f7b4ce19b8ecce74
Brian Olpin <bolpin@gmail.com>
2020-01-18 16:11:16 -0800

    formatting views/forms/dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
+  ActiveAdmin.routes(self)
 
   resources :disbursements
   resources :donations
+
+  root to: "home#index"
 end
