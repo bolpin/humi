0174c24a0c5f80bba9fae08994a44ac0d4ad76ba
Brian Olpin <bolpin@gmail.com>
2020-01-19 16:19:50 -0800

    css; error messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,10 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource_or_scope)
+    store_location_for(resource, admin_root_path)
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
     request.referrer
   end
 end
