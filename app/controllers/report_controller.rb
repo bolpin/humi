a87558b53dd0811ac45841c1364cd4ccbc4a6a9c
Brian Olpin <bolpin@gmail.com>
2020-01-25 11:02:55 -0800

    render instead of send_data

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,8 +1,8 @@
 class ReportController < ApplicationController
 
   def index
-    # render plain: json_report.to_s
-    send_data json_report.to_s, filename: "#{grant.name.gsub(/ /, '_')}.json"
+    render plain: json_report.to_s
+    # send_data json_report.to_s, filename: "#{grant.name.gsub(/ /, '_')}.json"
   end
 
   private
