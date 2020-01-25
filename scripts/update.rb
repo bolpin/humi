d015ed74581def3c80e36d0f547517786aa7c572
Brian Olpin <bolpin@gmail.com>
2020-01-25 08:56:10 -0800

    arguments to conversion script

--- scripts/update.rb
+++ scripts/update.rb
@@ -1,35 +1,45 @@
+#!/usr/bin/env ruby
+
 require 'json'
 require 'rubyXL'
 require 'rubyXL/convenience_methods/cell'
+
+if ARGV.length != 2
+  puts "Example usage: ruby json_to_excel.rb <partner1.json>"
+  exit
+end
+
+json_filename = ARGV[1]
 
 workbook = RubyXL::Parser.parse './humi-template.xlsx'
 
 DISBURSEMENTS_FIRST_ROW = 19
 DONATIONS_FIRST_ROW = 5 
 
-grant = JSON.parse(File.read('rpt.json'))
+grant = JSON.parse(File.read(json_filename))
 
-# (0..12).each do |month_index|
-#   new_donation_sheet = workbook.add_worksheet("donations #{month_index}")
-#
-#   grant['donations'][month_index.to_s].each_with_index do |donation, idx|
-#     new_donation_sheet.add_cell(idx, 0, donation['donor'])
-#     new_donation_sheet.add_cell(idx, 1, donation['date'])
-#     new_donation_sheet.add_cell(idx, 2, donation['amount'])
-#   end
-#
-#   new_disbursement_sheet = workbook.add_worksheet("disbursements #{month_index}")
-#
-#   grant['disbursements'][month_index.to_s].each_with_index do |disbursement, idx|
-#     row = new_disbursement_sheet[idx]
-#     new_disbursement_sheet.add_cell(idx, 0, disbursement['name'])
-#     new_disbursement_sheet.add_cell(idx, 1, disbursement['date'])
-#     new_disbursement_sheet.add_cell(idx, 2, disbursement['number_children'])
-#     new_disbursement_sheet.add_cell(idx, 3, disbursement['landlord'])
-#     new_disbursement_sheet.add_cell(idx, 4, disbursement['move_in_amount'])
-#     new_disbursement_sheet.add_cell(idx, 5, disbursement['prevention_amount'])
-#   end
-# end
+new_donation_sheet = workbook.add_worksheet("donations")
+new_disbursement_sheet = workbook.add_worksheet("disbursements #{month_index}")
+(0..12).each do |month_index|
+
+  grant['donations'][month_index.to_s].each_with_index do |donation, idx|
+    row = month_index+idx;
+    new_donation_sheet.add_cell(row, 0, donation['donor'])
+    new_donation_sheet.add_cell(row, 1, donation['date'])
+    new_donation_sheet.add_cell(row, 2, donation['amount'])
+  end
+
+  grant['disbursements'][month_index.to_s].each_with_index do |disbursement, idx|
+    row = month_index+idx;
+    new_disbursement_sheet.add_cell(row, 0, disbursement['name'])
+    new_disbursement_sheet.add_cell(row, 1, disbursement['date'])
+    new_disbursement_sheet.add_cell(row, 2, disbursement['number_children'])
+    new_disbursement_sheet.add_cell(row, 3, disbursement['landlord'])
+    new_disbursement_sheet.add_cell(row, 4, disbursement['move_in_amount'])
+    new_disbursement_sheet.add_cell(row, 5, disbursement['prevention_amount'])
+  end
+end
+
 
 (0..11).each do |month_index|
   worksheet = workbook[month_index + 2]
@@ -66,5 +76,5 @@ grant = JSON.parse(File.read('rpt.json'))
   end
 end
 
-workbook.write './test.xlsx'
+workbook.write "./#{json_filename}.xlsx"
 
